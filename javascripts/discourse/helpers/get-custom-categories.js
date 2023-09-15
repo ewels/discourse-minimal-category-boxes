a57d84ccb30dd38d2d8a989977ed627c18a670b6
Phil Ewels <phil.ewels@seqera.io>
2023-09-15 10:13:19 +0200

    Style tweaks to homepage box abbreviation letters

--- javascripts/discourse/helpers/get-custom-categories.js
+++ javascripts/discourse/helpers/get-custom-categories.js
@@ -4,12 +4,5 @@ import Category from "discourse/models/category";
 
 registerUnbound("getAbbreviation", categoryName => {
   let abbr = categoryName.replace(" and", "").split(" ");
-
-  if (abbr.length > 1) {
-    abbr = abbr[0].charAt(0).toUpperCase() + abbr[1].charAt(0).toLowerCase();
-  } else {
-    abbr = abbr[0].charAt(0).toUpperCase() + abbr[0].charAt(1).toLowerCase();
-  }
-
-  return abbr;
+  return abbr[0].charAt(0).toUpperCase();
 });
